eb8bdd962aee753c5076fd87e1ca53266d0f878e
Mozzammel Ridoy <mozzammelridoy5iii@gmail.com>
2024-05-22 16:58:32 +0600

    vercel config file added

--- index.js
+++ index.js
@@ -7,8 +7,17 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const corsOptions = {
+  origin: [
+      'http://localhost:5173',
+     
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
 // middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 //Mongodb Start
