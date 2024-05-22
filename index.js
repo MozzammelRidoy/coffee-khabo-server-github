30db4caf91991941077b82946630c0ea927c7420
Mozzammel Ridoy <mozzammelridoy5iii@gmail.com>
2024-05-22 17:01:48 +0600

    vercel config file added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 const corsOptions = {
   origin: [
-      'http://localhost:5173',
+      'http://localhost:5173'
      
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
